136fdaf12962c0f2e663e38eee9b62ad62810ca5
Tomas Forsman <tomas@forsman.dev>
2020-06-01 16:21:05 +0200

    Bugfix after merge

--- Tailspin.SpaceGame.Web/Startup.cs
+++ Tailspin.SpaceGame.Web/Startup.cs
@@ -5,11 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TailSpin.SpaceGame.Web.Models;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 
 namespace TailSpin.SpaceGame.Web
 {
